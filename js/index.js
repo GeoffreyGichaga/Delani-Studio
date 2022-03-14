027889624f06c0b8530012e753574e450a29cd9d
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-03-14 04:28:46 +0300

    chore : filter - filter brightness of the images on portfolio when hoovered

--- js/index.js
+++ js/index.js
@@ -41,39 +41,53 @@ $(document).ready(()=>{
     $('#img2').mouseenter(()=>
     {
         $('.cols-info2').slideToggle();
+        $('#img2').addClass("darken")
+
     })
 
     $('#img3').mouseenter(()=>
     {
         $('.cols-info3').slideToggle();
+        $('#img3').addClass("darken")
+
     })
 
     $('#img4').mouseenter(()=>
     {
         $('.cols-info4').slideToggle();
+        $('#img4').addClass("darken")
+
     })
 
     $('#img5').mouseenter(()=>
     {
         $('.cols-info5').slideToggle();
+        $('#img5').addClass("darken")
+
     })
 
 
     $('#img6').mouseenter(()=>
     {
         $('.cols-info6').slideToggle();
+        $('#img6').addClass("darken")
+
     })
 
 
     $('#img7').mouseenter(()=>
     {
         $('.cols-info7').slideToggle();
+        $('#img7').addClass("darken")
+
     })
 
 
     $('#img8').mouseenter(()=>
     {
         $('.cols-info8').slideToggle();
+        $('#img8').addClass("darken")
+
     })
 
 
